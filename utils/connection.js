44cd4b0e0c676d2a2d3e47828ca5fded7d7a573e
Fakhrul <barz.develop@gmail.com>
2024-05-11 22:47:19 +0700

    add: connection to mongo cloud

--- utils/connection.js
+++ utils/connection.js
@@ -4,10 +4,16 @@ const dotenv = require("dotenv");
 dotenv.config();
 const localhost = "mongodb://127.0.0.1:27017/test";
 const mongoUri = process.env.MONGO_URI;
+const uri = mongoUri ? mongoUri : localhost;
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
 
 async function connectDB() {
   try {
-    await mongoose.connect(mongoUri ? mongoUri : localhost);
+    await mongoose.connect(uri, connectionParams);
     console.log("DB Connected");
   } catch (err) {
     console.error("DB Connection Error:", err);
